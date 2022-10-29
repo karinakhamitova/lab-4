f155b12894d286cefbf29983e5d7e0d878f34a2e
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-10-29 17:09:02 +0300

    Добавьте файлы проекта.

---
+++ Задание 4_Хамитова/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4_Хамитова
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int N = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                N = N+2 * i - 1;
+                Console.WriteLine(N);
+            }
+            Console.ReadKey();
+        }
+    }
+}
